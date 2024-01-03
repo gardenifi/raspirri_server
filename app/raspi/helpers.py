be2cb1b069907be809ba69829a9d30e93da28f9b
Marios Karagiannopoulos <mariosk@gmail.com>
2024-01-03 07:40:34 +0000

    Fixing pre-commit issues

--- app/raspi/helpers.py
+++ app/raspi/helpers.py
@@ -230,7 +230,7 @@ class Helpers:
 
         # Open the file in read mode ('r')
         try:
-            with open(file_path) as file:
+            with open(file_path, 'r', encoding='utf-8') as file:
                 # Read the entire content of the file
                 # content = file.read()
 
