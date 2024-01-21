b89ec636d05b3dd660095cc6917d46f5f9e0e873
Marios Karagiannopoulos <mariosk@gmail.com>
2024-01-21 16:53:51 +0000

    11 # Fixing release.yaml

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 """File needed in release bumps through Github actions"""
 import os
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
 
 def read_requirements():
@@ -10,6 +11,34 @@ def read_requirements():
         return [line.strip() for line in f if line.strip()]
 
 
+class CustomInstallCommand(install):
+    """Custom install command that executes install.sh script after installation."""
+
+    def run(self):
+        """Run the custom install command."""
+        install.run(self)
+        self.run_custom_install()
+
+    def run_custom_install(self):
+        """Execute the install.sh script."""
+        install_script_path = os.path.join(os.path.dirname(__file__), "install.sh")
+        os.system(f"bash {install_script_path}")
+
+
+class CustomUninstallCommand(install):
+    """Custom develop command that executes uninstall.sh script after development un-installation."""
+
+    def run(self):
+        """Run the custom uninstall command."""
+        install.run(self)
+        self.run_custom_uninstall()
+
+    def run_custom_uninstall(self):
+        """Execute the uninstall.sh script."""
+        uninstall_script_path = os.path.join(os.path.dirname(__file__), "uninstall.sh")
+        os.system(f"bash {uninstall_script_path}")
+
+
 setup(
     name="raspirri-server",
     version=os.getenv("NEW_VERSION", None),
@@ -17,4 +46,8 @@ setup(
     author="Marios Karagiannopoulos (mariosk@gmail.com)",
     packages=find_packages(),
     install_requires=read_requirements(),
+    cmdclass={
+        "install": CustomInstallCommand,
+        "uninstall": CustomUninstallCommand,
+    },
 )
