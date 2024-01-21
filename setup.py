88c3fc657a07022b73c86251f9814b5ec1aa0284
Marios Karagiannopoulos <mariosk@gmail.com>
2024-01-21 14:07:50 +0000

    11 # Fixing release.yaml

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 """File needed in release bumps through Github actions"""
+import os
 from setuptools import setup, find_packages
+
+
+def read_requirements():
+    """Read requirements from requirements.txt file"""
+    requirements_path = os.path.join(os.path.dirname(__file__), "requirements.txt.arm")
+    with open(requirements_path, encoding="utf-8") as f:
+        return [line.strip() for line in f if line.strip()]
+
 
 setup(
     name="raspirri-server",
-    version="0.1.0",
+    version=os.getenv("NEW_VERSION", None),
     description="RaspirriV1 Server",
     author="Marios Karagiannopoulos (mariosk@gmail.com)",
     packages=find_packages(),
-    install_requires=[
-        # List your dependencies here
-    ],
+    install_requires=read_requirements(),
+    package_data={
+        "raspirri-server": ["*.service", "*.md", "*.sh", "certs/*.pem"],
+    },
+    include_package_data=True,
 )
