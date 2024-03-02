daf5b116f454502899882c188dfb135ae6b1e0e7
Marios Karagiannopoulos <mariosk@gmail.com>
2024-03-02 09:37:48 +0000

    Fixing new_release.sh

--- new_release.sh
+++ new_release.sh
@@ -4,5 +4,5 @@ pip install bump2version
 export NEW_VERSION=${NEW_VERSION}
 export PART=patch
 bump2version --new-version ${NEW_VERSION} ${PART} --config-file bumpversion.cfg
-git add pubspec.yaml && git commit -m "Bumping version to v${NEW_VERSION}" && git push
+git add bumpversion.cfg && git commit -m "Bumping version to v${NEW_VERSION}" && git push
 git tag "v${NEW_VERSION}" && git push origin "v${NEW_VERSION}"
